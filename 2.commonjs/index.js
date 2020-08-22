85ff22937beb8f78dc44f79774a418d543d658b5
Torry <76385651@qq.com>
2020-08-23 01:48:41 +0800

    update

---
+++ 2.commonjs/index.js
@@ -0,0 +1,18 @@
+// 石头剪刀布游戏
+const game = require('./lib')
+
+let count = 0;
+
+process.stdin.on('data', e => {
+	const playerAction = e.toString().trim()
+	const result = game(playerAction)
+	console.log(result)
+
+	if (result < 0) {
+		count++;
+	}
+	if (count === 3) {
+		console.log('你太厉害了，我不玩了')
+		process.exit();
+	}
+})
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # nodejs-geektime-demo
+
+## Node.js版Geektime
+### 列表页
+- 打通前后台
+- 服务端渲染
+### 详情页
+- 网页路由
+- 异步加载
+### 播放页
+- API服务器
+
+
+## BFF层
+Backend for Frontend
+### 主要职责
+- 对用户侧提供HTTP服务
+- 使用后端RPC服务
+
+
+## CommonJS模块规范
+webpack --devtool none --mode development --target node index.js
