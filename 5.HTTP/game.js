69c684f7c920d04c393c1d1c01cbfba8497bc97d
陶莉 <76385651@qq.com>
2021-08-08 22:41:37 +0800

    update notes to lesson 22

--- 5.HTTP/game.js
+++ 5.HTTP/game.js
@@ -1,4 +1,9 @@
 module.exports = function(playerAction) {
+
+  if (['rock', 'scissor', 'paper'].indexOf(playerAction) == -1) {
+    throw new Error('invalid playerAction');
+  }
+
   let random = Math.random() * 3;
 
 	let computerAction;
@@ -10,20 +15,20 @@ module.exports = function(playerAction) {
 	} else {
 		computerAction = 'paper'
 	}
-	console.log('我出了' + computerAction)
+	// console.log('我出了' + computerAction)
 
 	if (computerAction === playerAction) {
-		console.log('平局')
+		// console.log('平局')
 		return 0;
 	} else if (
 		(computerAction === 'rock' && playerAction === 'paper') || 
 		(computerAction === 'scissor' && playerAction === 'rock') ||
 		(computerAction === 'paper' && playerAction === 'scissor')
 	) {
-		console.log('你赢了');
+		// console.log('你赢了');
 		return -1;
 	} else {
-		console.log('你输了');
+		// console.log('你输了');
 		return 1;
 	}
 }
--- 5.HTTP/index.js
+++ 5.HTTP/index.js
@@ -1,13 +1,64 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
+const querystring = require('querystring');
+const game = require('./game');
+
+let playWon = 0;
+
+let playerLastAction = null;
+let sameCount = 0;
 
 http.createServer(function(req, res) {
-  if (req.url === '/favicon.ico') {
+  const parsedUrl = url.parse(req.url);
+  
+  if (parsedUrl.pathname === '/favicon.ico') {
     res.writeHead(200);
     res.end();
     return;
   }
+
+  if (parsedUrl.pathname === '/game') {
+    const query = querystring.parse(parsedUrl.query);
+    const playerAction = query.action;
+
+    if (playWon >= 3 || sameCount === 9) {
+      res.writeHead(500);
+      res.end('我再也不和你玩了！')
+      return;
+    }
+
+    if (playerLastAction && playerLastAction === playerAction) {
+      sameCount += 1;
+    } else {
+      sameCount = 0;
+    }
+
+    playerLastAction = playerAction;
+
+    if (sameCount >=3) {
+      res.writeHead(400);
+      res.end('你作弊！')
+      sameCount = 9;
+      return;
+    }
+
+    const gameResult = game(playerAction);
+    
+    res.writeHead(200);
+    if (gameResult == 0) {
+      res.end('平局！');
+    } else if (gameResult == 1) {
+      playWon += 1;
+      res.end('你赢了！');
+
+    } else {
+      res.end('你输了！');
+    }
+  }
   
-  res.writeHead(200);
-  fs.createReadStream(__dirname+'/index.html').pipe(res);
+  if (parsedUrl.pathname === '/') {
+    fs.createReadStream(__dirname+'/index.html').pipe(res);
+  }
+  
 }).listen(3000);
