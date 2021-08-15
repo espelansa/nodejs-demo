7644e4a0cf81224d6f7193d61fc4b7209bce48f4
陶莉 <76385651@qq.com>
2021-08-15 21:55:03 +0800

    finish notes to lesson 25

--- 6. RPC/client.js
+++ 6. RPC/client.js
@@ -1,10 +1,69 @@
 const net = require('net');
 
-const socket = new net.Socket({});
+// const socket1 = new net.Socket({});
 
-socket.connect({
+// socket1.connect({
+//   host: '127.0.0.1',
+//   port: 4000
+// });
+
+// socket1.write('good evening');
+
+
+const lessonIds = [
+  136797,
+  136798,
+  136799,
+  136800
+]
+
+const socket2 = new net.Socket({});
+
+let seq = 0;
+
+let id = Math.floor(Math.random() * lessonIds.length);
+
+socket2.connect({
   host: '127.0.0.1',
-  port: 4000
+  port: 4001
 });
 
-socket.write('good evening')
+socket2.write(encode(id));
+
+socket2.on('data', buffer => {
+  
+  const seqBuffer = buffer.slice(0, 2);
+  const titleBuffer = buffer.slice(2);
+
+  console.log(seqBuffer.readInt16BE(), titleBuffer.toString());
+
+  //  id = Math.floor(Math.random() * lessonIds.length);
+  //  socket2.write(encode(id));
+})
+
+function encode(index) {
+  let buffer = Buffer.alloc(6);
+  buffer.writeInt16BE(seq);
+  buffer.writeInt32BE(
+    lessonIds[index], 2
+  )
+  console.log(seq, lessonIds[index]);
+  seq++;
+  return buffer;
+}
+
+// 持续发包，收到包的时间也不确定
+// setInterval(function() {
+//   id = Math.floor(Math.random() * lessonIds.length);
+//   socket2.write(encode(id));
+// }, 100)
+
+// 同时把100个包发出去
+for (let k = 0; k < 100; k++) {
+  id = Math.floor(Math.random() * lessonIds.length);
+  socket2.write(encode(id));
+}
+
+// 结果是只收到2个包的结果
+// TCP的底层机制导致的，会自动把我们同时发的一些包拼接起来，拼成一个大包，一次性发给网络另一端
+// 涉及到粘包切分逻辑
--- 6. RPC/server.js
+++ 6. RPC/server.js
@@ -1,15 +1,42 @@
 const net = require('net');
 
-const server = net.createServer(socket => {
-  // socket在计算机网络编程里一般代表这个网络写入和读取的代理的对象
-  socket.on('data', function(buffer) {
-    console.log(buffer, buffer.toString())
-  })
-});
+// const server1 = net.createServer(socket => {
+//   // socket在计算机网络编程里一般代表这个网络写入和读取的代理的对象
+//   socket.on('data', function(buffer) {
+//     console.log(buffer, buffer.toString())
+//   })
+// });
 
-server.listen(4000);
+// server1.listen(4000);
 
 /**
- * server没有启动前 node client 会报错
+ * server没有启动前 node client.js会报错
+ * server启动后，node client.js会自动出现如下字符：
  * <Buffer 67 6f 6f 64 20 65 76 65 6e 69 6e 67> good evening
  */
+
+const server2 = net.createServer(socket => {
+
+  socket.on('data', function(buffer) {
+    const seqBuffer = buffer.slice(0, 2); 
+    const lessonId = buffer.readInt32BE(2);
+
+    setTimeout(() => {
+      const buffer = Buffer.concat([
+        seqBuffer,
+        Buffer.from(data[lessonId])
+      ]);
+      socket.write(buffer);
+    }, 10 + Math.random() * 1000);
+    
+  })
+})
+
+server2.listen(4001);
+
+const data = {
+  136797: "01 | 课程介绍",
+  136798: "02 | 内容综述",
+  136799: "03 | Node.js是什么？",
+  136800: "04 | Node.js可以用来做什么？"
+}
